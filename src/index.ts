f01debb9292d627c53e5a1723aaf266c788c0605
hadiazb <hadiazb@unal.edu.co>
2023-01-14 06:28:28 -0500

    start project

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, NextFunction, Response } from 'express';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+	res.send({
+		Hola: 'mundo',
+	});
+});
+
+app.listen(process.env.PORT, () => {
+	console.log(`Running on ${process.env.PORT}`);
+});
